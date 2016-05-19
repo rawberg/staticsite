bcbe274afe8d511d5a563ee29df4dd0994c015fb
David Feinberg <david@rawberg.com>
2016-05-19 07:49:52 -0700

    rackspace cloud storage support with custom directory uploads

---
+++ clouds/rkp.js
@@ -0,0 +1,85 @@
+var fs = require('fs'),
+    path = require('path'),
+    walk = require('walk');
+
+
+module.exports.createSite = function(options) {
+    "use strict";
+
+    var required = ["user", "apiKey", "callback"];
+    for(let req of required) {
+        if(typeof options[req] === "undefined" || options[req] === "") {
+            throw("required " + req + ", not provided");
+        }
+    }
+
+    var client = require("pkgcloud").providers.rackspace.storage.createClient({
+        username: options.user,
+        apiKey: options.apiKey,
+        region: options.region
+    });
+
+    client.createContainer({
+        name: options.domain,
+        metadata: {
+            "X-Container-Meta-Web-Index": "index.html"
+        }
+    }, function(error, data) {
+        if(error) {
+            throw(error);
+        } else {
+            try {
+                var uploadTasks = [];
+                var walker = walk.walk(options.directory, { followLinks: true });
+                walker.on("file", function(root, fileStat, next) {
+                    var cloudPath = fileStat.name;
+                    if(root.length > options.directory.length) {
+                        cloudPath = root.substr(options.directory.length + 1) + "/" + fileStat.name;
+                    }
+                    console.log("cloudPath: ", cloudPath);
+                    uploadTasks.push(new Promise((resolve, reject) => {
+                        var writeStream = client.upload({
+                            container: options.domain,
+                            remote: cloudPath
+                        });
+                        writeStream.on("finish", resolve);
+                        writeStream.on("error", reject);
+                        fs.createReadStream(path.join(root, fileStat.name)).pipe(writeStream);
+                    }));
+                    next();
+                });
+
+                walker.on("errors", function(root, nodeStatsArray, next) {
+                    nodeStatsArray.forEach(function (n) {
+                        throw(n.error.message || (n.error.code + ": " + n.error.path));
+                    });
+                    next();
+                });
+
+            } catch(error) {
+                throw(error);
+            }
+            Promise.all(uploadTasks)
+                .then(options.callback)
+                .catch(function(error) { throw(error); });
+        }
+    });
+};
+
+module.exports.getContainers = function(options) {
+    "use strict";
+
+    var required = ["user", "apiKey", "callback"];
+    for(let req of required) {
+        if(typeof options[req] === "undefined" || options[req] === "") {
+            throw("required " + req + ", not provided");
+        }
+    }
+
+    var client = require("pkgcloud").providers.rackspace.storage.createClient({
+        username: options.user,
+        apiKey: options.apiKey,
+        region: options.region
+    });
+    client.getContainers(options.callback);
+};
