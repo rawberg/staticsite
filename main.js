232b3f1e11a41b598097af52de6b223a42020878
David Feinberg <david@rawberg.com>
2016-05-19 07:53:53 -0700

    refactored core CLI functionality added rackspace support

--- main.js
+++ main.js
@@ -2,108 +2,69 @@
 "use strict";
 
 var argparse = require('argparse'),
-    rkp = require("./rkp"),
-    aws = require("./aws");
+    rkp = require("./clouds/rkp"),
+    aws = require("./clouds/aws"),
+    azure = require("./clouds/azure"),
+    validations = require("./validations");
 
-var usageExamples = "examples\nAWS\n staticsite aws new-site.com -k aws-secret-key -p aws-secret-password -r us-west-2 (defaults to us-east-1)\n" +
-    "Rackspace\n staticsite rkp new-site.com -k rackspace-api-key\n" +
-    "Azure\n staticsite azure new-site.com -k azure-secret-key -p azure-secret-password";
+var usageExamples = "examples\n" +
+    "AWS\n staticsite aws new-site.com" +
+    " -k aws-secret-key -p aws-secret-password -r us-west-2 " +
+    "(defaults to us-east-1)\n Rackspace\n staticsite " +
+    "rkp new-site.com -u rkp-username -k rkp-api-key\n" +
+    "Azure\n staticsite azure new-site.com -k azure-secret-key " +
+    "-p azure-secret-password";
 
 
 var parser = new argparse.ArgumentParser({
-    version: "0.0.1",
+    version: "0.99.0",
     addHelp: true,
     formatterClass: argparse.RawTextHelpFormatter,
     usage: usageExamples,
     description: "StaticSite CLI"
 });
 
-parser.addArgument("cloud", {
-    help: "cloud platform [aws, rkp, azure]",
-    choices: ["aws", "rkp", "azure"]
-});
-
-parser.addArgument("domain", {
-    help: "site domain name [mynewsite.com]"
-});
-
-parser.addArgument(["-r", "--region"], {
-    help: "    bucket region (aws only)\n",
-    metavar: "\b",
-    required: false
-});
-
-parser.addArgument(["-k", "--key"], {
-    help: "    aws: secret access key (required)\nrkp: api key\nazure: secret azure thingy\n",
-    metavar: "\b",
-    required: false
-});
-
-parser.addArgument(["-p", "--pass"], {
-    help: "    aws: secret password (required)\nrkp: not used",
-    metavar: "\b",
-    required: false
-});
-
+// load up avialable arguments
+require("./arguments")(parser);
 var args = parser.parseArgs();
 
-console.log(args);
+// DEBUG
+// console.log(args);
 
-function awsValidations() {
-    var awsRegions = [
-        "us-east-1",
-        "us-west-1",
-        "us-west-2",
-        "eu-central-1",
-        "ap-northeast-1",
-        "ap-northeast-2",
-        "ap-southeast-1",
-        "ap-southeast-2",
-        "sa-east-1"
-    ];
-
-    domainValidation();
-    if(args.key === null || args.key === "") {
-        console.log("missing required aws secret access key");
-        process.exit(1);
-    }
-    if(args.pass === null || args.pass === "") {
-        console.log("missing required aws password");
-        process.exit(1);
-    }
-    if(args.region !== null || args.region !== "") {
-        if(awsRegions.indexOf(args.region) < 0) {
-            console.log("invalid aws region, available regions: " + awsRegions.join(", "));
-            process.exit(1);
-        }
-    }
-}
-
-function domainValidation() {
-    if(!(/\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/.test(args.domain))) {
-        console.log("invalid domain name (" + args.domain + ")");
-        process.exit(1);
-    }
-}
-
+// handle cloud command
 switch(args.cloud) {
     case "aws":
-        awsValidations();
+        validations.awsValidations(args);
         aws.createSite({
             awsKey: args.key,
             awsPass: args.pass,
             domain: args.domain,
+            directory: args.directory,
             callback: function(error, data) {
                 if(error) {
-                    console.log(error);
+                    console.log("aws error: ", error);
                 } else {
-                    console.log("site created!");
+                    console.log("successfully created " + args.domain + " on amazon web services cloud.");
                 }
             }
         });
         break;
     case "rkp":
-        console.log("rackspace is in progress");
+        validations.rkpValidations(args);
+        rkp.createSite({
+            user: args.user,
+            apiKey: args.key,
+            region: args.region,
+            domain: args.domain,
+            directory: args.directory,
+            callback: function(error) {
+                if(error) {
+                    console.log("rkp error: ", error);
+                } else {
+                    console.log("successfully created " + args.domain + " on rackspace cloud.");
+                }
+            }
+        });
         break;
     case "azure":
         console.log("azure is coming soon");
