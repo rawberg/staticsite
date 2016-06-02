e26824651f0c56c834c4bb7f34714a931bf4c33e
David Feinberg <david@rawberg.com>
2016-06-02 09:20:13 -0700

    - add index.js and update package.json to allow usage as a module

---
+++ index.js
@@ -0,0 +1,7 @@
+var aws = require("./clouds/aws"),
+    azure = require("./clouds/azure"),
+    rkp = require("./clouds/rkp");
+
+module.exports.aws = aws;
+module.exports.azure = azure;
+module.exports.rkp = rkp;
